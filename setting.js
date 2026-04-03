49d14a3d1f3aa6b50cba270fde11afde20235312
kiranchaudhari0096-arch <kiranchaudhari0096@gmail.com>
2026-04-03 16:58:25 +0530

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,8 +1,10 @@
 module.exports = {
   uiPort: process.env.PORT || 8080,
+  uiHost: "0.0.0.0",   
   httpAdminRoot: "/",
   httpNodeRoot: "/api",
   userDir: "/root/.node-red",
   flowFile: "flows.json",
+  credentialSecret: "my-secret-key",
   functionGlobalContext: {}
 };
