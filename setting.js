123c404da138b08d9134f87f73517fa8e5732b9c
kiranchaudhari0096-arch <kiranchaudhari0096@gmail.com>
2026-04-03 16:51:20 +0530

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,8 @@
+module.exports = {
+  uiPort: process.env.PORT || 8080,
+  httpAdminRoot: "/",
+  httpNodeRoot: "/api",
+  userDir: "/root/.node-red",
+  flowFile: "flows.json",
+  functionGlobalContext: {}
+};
